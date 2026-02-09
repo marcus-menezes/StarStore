812371d98e01b0d31316d335250678ab7c6c0b95
Marcus Menezes <marcusmenezes2009@gmail.com>
2026-02-08 22:08:04 -0300

    Add index file to export Firebase and storage services, including authentication methods and secure storage utilities.

---
+++ mobile/services/index.ts
@@ -0,0 +1,15 @@
+export {
+  signIn,
+  signUp,
+  signOut,
+  onAuthStateChanged,
+  getCurrentUser,
+  db,
+  getCollection,
+  getDocument,
+  COLLECTIONS,
+  auth,
+  firestore,
+} from './firebase';
+
+export { SecureStorage, Storage, STORAGE_KEYS } from './storage';
