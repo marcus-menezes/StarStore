bd7fa52a442978c587026b666388bb62c6ad4a09
Marcus Menezes <marcusmenezes2009@gmail.com>
2026-02-08 22:07:44 -0300

    Add index file to export custom hooks for color scheme, client-only values, products, orders, and authentication management.

---
+++ mobile/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useColorScheme } from './useColorScheme';
+export { useClientOnlyValue } from './useClientOnlyValue';
+export { useProducts, useProduct } from './useProducts';
+export { useOrders, useCreateOrder } from './useOrders';
+export { useAuth } from './useAuth';
