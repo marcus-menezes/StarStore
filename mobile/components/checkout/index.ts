adad850307c154aad1f9c1f0f66e439f6d497acb
Marcus Menezes <marcusmenezes2009@gmail.com>
2026-02-10 06:30:58 -0300

    Add checkout component exports for payment processing
    - Introduce a new index file in the checkout component to export CreditCardVisual, CreditCardForm, PaymentMethodSelector, PaymentInfoCard, and OrderSummary components for streamlined access and organization.

---
+++ mobile/components/checkout/index.ts
@@ -0,0 +1,5 @@
+export { CreditCardVisual } from './CreditCardVisual';
+export { CreditCardForm } from './CreditCardForm';
+export { PaymentMethodSelector } from './PaymentMethodSelector';
+export { PaymentInfoCard } from './PaymentInfoCard';
+export { OrderSummary } from './OrderSummary';
