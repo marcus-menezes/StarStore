01d93ed6c2a3a49567db312c15bb083a3449724e
Marcus Menezes <marcusmenezes2009@gmail.com>
2026-02-10 04:28:28 -0300

    Refactor withCrashlyticsConfig.js to use consistent single quotes and improve readability of manifest modifications.

--- mobile/plugins/withCrashlyticsConfig.js
+++ mobile/plugins/withCrashlyticsConfig.js
@@ -1,4 +1,4 @@
-const { withAndroidManifest } = require("@expo/config-plugins");
+const { withAndroidManifest } = require('@expo/config-plugins');
 
 /**
  * Custom Expo config plugin to add Firebase Crashlytics meta-data
@@ -13,29 +13,24 @@ function withCrashlyticsConfig(config) {
     const mainApplication = config.modResults.manifest.application[0];
 
     // Add xmlns:tools if not present
-    if (!config.modResults.manifest.$["xmlns:tools"]) {
-      config.modResults.manifest.$["xmlns:tools"] =
-        "http://schemas.android.com/tools";
+    if (!config.modResults.manifest.$['xmlns:tools']) {
+      config.modResults.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
     }
 
     // Add crashlytics meta-data
-    if (!mainApplication["meta-data"]) {
-      mainApplication["meta-data"] = [];
+    if (!mainApplication['meta-data']) {
+      mainApplication['meta-data'] = [];
     }
 
-    const metaData = mainApplication["meta-data"];
+    const metaData = mainApplication['meta-data'];
 
     // firebase_crashlytics_collection_enabled
-    if (
-      !metaData.find(
-        (m) => m.$["android:name"] === "firebase_crashlytics_collection_enabled",
-      )
-    ) {
+    if (!metaData.find((m) => m.$['android:name'] === 'firebase_crashlytics_collection_enabled')) {
       metaData.push({
         $: {
-          "android:name": "firebase_crashlytics_collection_enabled",
-          "android:value": "true",
-          "tools:replace": "android:value",
+          'android:name': 'firebase_crashlytics_collection_enabled',
+          'android:value': 'true',
+          'tools:replace': 'android:value',
         },
       });
     }
